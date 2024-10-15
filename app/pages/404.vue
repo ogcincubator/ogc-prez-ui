10945334067eac0a2c5bed49c31de04e03d899d1
Alejandro Villar <avillar@ogc.org>
2024-10-15 10:41:40 +0200

    Add 404 page

---
+++ app/pages/404.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const route = useRoute();
+const resourceUri = route.query.uri;
+</script>
+<template>
+  <NuxtLayout contentonly>
+    <template #default>
+      <h1 class="text-2xl mt-8">Resource not found</h1>
+      <p v-if="resourceUri">Sorry, the requested resource (<code>{{ resourceUri }}</code>) could not be found.</p>
+      <p v-else>Sorry, the requested resource could not be found.</p>
+      <NuxtLink to="/">Go home</NuxtLink>
+    </template>
+  </NuxtLayout>
+</template>
