bcdf84c6950d84de748d00110c5dc97d7aac5919
Alejandro Villar <avillar@ogc.org>
2024-10-31 10:54:39 +0100

    Change "Catalogs" to "Browse" in menu

---
+++ app/app.config.ts
@@ -0,0 +1,8 @@
+export default defineAppConfig({
+  menu: () => [
+    {"label": "Home", "url": "/"},
+    {"label": "Browse", "url": "/catalogs"},
+    {"label": "Search", "url": "/search"},
+    {"label": "About", "url": "/about"},
+  ],
+});
