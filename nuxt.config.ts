0a256ee19c7cba6c0d62074631b9e1e134c108a1
MarkusWilhelmJahn <85496910+MarkusWilhelmJahn@users.noreply.github.com>
2025-05-08 08:05:16 +0200

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    compatibilityDate: "2024-04-03",
+    devtools: { enabled: true },
+    modules: [
+      "@nuxtjs/tailwindcss",
+      "shadcn-nuxt",
+      "@nuxtjs/color-mode",
+    ],
+    extends: [
+        "prez-ui"
+    ],
+    vite: {
+        optimizeDeps: {
+            include: ["@triply/yasgui"]
+        }
+    },
+});
