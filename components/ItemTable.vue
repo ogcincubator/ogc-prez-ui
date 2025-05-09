c82ca72dafb8d17516927698af7ce781f5e8152e
Alejandro Villar <avillar@ogc.org>
2025-05-09 13:57:55 +0200

    Redo ItemTable to just override term properties

--- components/ItemTable.vue
+++ components/ItemTable.vue
@@ -1,30 +1,23 @@
 <script lang="ts" setup>
-import { type PrezFocusNode } from 'prez-lib';
-import { ItemTableRow ,type ItemTableProps } from 'prez-components';
-import { Table, TableBody } from '@/components/ui/table';
+import ItemTable from 'prez-ui/components/ItemTable.vue';
+import type {ItemTableProps} from "prez-components";
+import {type PrezFocusNode} from 'prez-lib';
 
 const props = defineProps<ItemTableProps>();
-const term = props.term as PrezFocusNode;
 
-const runtimeConfig = useRuntimeConfig();
-
-// const fieldNames = Object.keys(term.properties || {});
-
-// const fields = computed(()=>
-//     [...(props.fields || []).filter(f => fieldNames.includes(f.node.value)).map(f=>f.node.value),    // add fields that are in the list
-//     ...fieldNames.filter(fname => !(props.fields || []).find(f=>f.node.value == fname))              // add the rest of the fields that are not in the list
-//     ].filter(f=>f in (term.properties || {})).map(f=>term.properties![f] as PrezProperty)
-// );
 const hideSingleObjectPredicates = [
     'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
     'http://www.w3.org/2004/02/skos/core#prefLabel',
     'http://www.w3.org/2000/01/rdf-schema#label',
+    'http://purl.org/dc/terms/title',
 ];
 const hidePredicates = [
     'http://www.opengis.net/ogc-na#targetGraph',
 ];
-const filteredFields = computed(() => {
-  if (!term.properties) {
+const term = props.term as PrezFocusNode;
+
+const filteredProperties = computed(() => {
+  if (!term?.properties) {
     return [];
   }
   return Object.values(term.properties).filter(f => {
@@ -37,22 +30,20 @@ const filteredFields = computed(() => {
     return true;
   });
 });
+
+const filteredProps = computed(() => {
+  return {
+    ...props,
+    term: {
+      ...props.term,
+      properties: filteredProperties.value,
+    },
+  }
+});
 </script>
 
 <template>
-    <!-- ItemTable -->
-    <Table v-if="term?.properties" class="item-table">
-        <TableBody role="rowgroup">
-            <component :is="ItemTableRow" v-for="(fieldProp, index) in filteredFields"
-                :key="fieldProp?.predicate.value"
-                :index="index"
-                :term="term"
-                :objects="fieldProp ? fieldProp.objects : []"
-                :predicate="fieldProp!.predicate"
-                :renderHtml="!!runtimeConfig.public.prezAutoDetectHtml"
-                :renderMarkdown="!!runtimeConfig.public.prezAutoDetectMarkdown"
-            />
-        </TableBody>
-    </Table>
+  <ItemTable
+      v-bind="filteredProps"
+  />
 </template>
-
