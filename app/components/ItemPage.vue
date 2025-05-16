9757fd34ea5d65b893f3b93ae188e8d9dc018b2e
Alejandro Villar <avillar@ogc.org>
2025-05-16 09:50:54 +0200

    Hide members button in Concept, and only fetch members if required

--- app/components/ItemPage.vue
+++ app/components/ItemPage.vue
@@ -13,10 +13,14 @@ const SHOW_MEMBER_LIST_CLASSES = [
   'http://www.w3.org/2004/02/skos/core#Collection',
   'http://www.w3.org/2002/07/owl#Ontology',
 ]
+const HIDE_MEMBERS_BUTTON_CLASSES = [
+  'http://www.w3.org/2004/02/skos/core#Concept',
+]
 
 const urlPath = ref(getPageUrl());
 const {status, error, data} = useGetItem(runtimeConfig.public.prezApiEndpoint, urlPath);
 const showMembersTable = computed(() => data.value?.data.rdfTypes?.find(n => SHOW_MEMBER_LIST_CLASSES.includes(n.value)));
+const hideMembersButton = computed(() => data.value?.data.rdfTypes?.find(n => HIDE_MEMBERS_BUTTON_CLASSES.includes(n.value)));
 const membersUrl = computed(() => {
   const baseMembersUrl = data.value?.data?.members?.value;
   return baseMembersUrl ? baseMembersUrl + getPageUrl().replace(/^[^?]+/, '') : '';
@@ -24,21 +28,23 @@ const membersUrl = computed(() => {
 
 // Workaround for when membersUrl is not yet ready and thus null
 const membersData = shallowRef(null);
+const membersError = shallowRef(null);
 watch(membersUrl, () => {
-  if (membersData.value || !membersUrl.value) {
+  if (membersData.value || !membersUrl.value || !showMembersTable) {
     return;
   }
-  const {data} = useGetList(runtimeConfig.public.prezApiEndpoint, membersUrl);
+  const {data, error} = useGetList(runtimeConfig.public.prezApiEndpoint, membersUrl);
   membersData.value = data.value;
 
   watch(data, v => membersData.value = v);
+  watch(error, v => { membersError.value = v });
 }, {
   immediate: true,
 });
 </script>
 <template>
   <ItemPage>
-    <template #item-members v-if="!!membersUrl && showMembersTable">
+    <template #item-members v-if="!!membersUrl && showMembersTable && !membersError">
       <h2 class="font-semibold pt-2">Members of {{ data?.data?.label?.value || data!.data.value }}</h2>
       <div v-if="membersData?.data">
         <ItemList :list="membersData.data" :key="membersData"/>
@@ -59,5 +65,6 @@ watch(membersUrl, () => {
       </div>
       <Loading variant="item-table" v-else></Loading>
     </template>
+    <template #item-members v-if="hideMembersButton"><span class="hidden"></span></template>
   </ItemPage>
 </template>
