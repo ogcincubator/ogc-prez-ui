470ec0b1a6e8dfcebf60378576e0e9e1fa7f77b1
MarkusWilhelmJahn <85496910+MarkusWilhelmJahn@users.noreply.github.com>
2025-05-19 14:39:34 +0200

    Integrated Updates of ogc-prez-ui

--- components/ItemPage.vue
+++ components/ItemPage.vue
@@ -1,43 +1,63 @@
 <script setup lang="ts">
 import ItemPage from 'prez-ui/components/ItemPage.vue';
 import ItemList from 'prez-ui/components/ItemList.vue';
+import type {ShallowRef} from "@vue/reactivity";
 
 const runtimeConfig = useRuntimeConfig();
 const route = useRoute();
 const {getPageUrl, pagination} = usePageInfo();
 
+const SHOW_MEMBER_LIST_CLASSES = [
+  'http://www.w3.org/ns/dcat#Catalog',
+  'http://www.w3.org/ns/dcat#TopCatalog',
+  'http://www.w3.org/2004/02/skos/core#Collection',
+  'http://www.w3.org/2002/07/owl#Ontology',
+]
+const HIDE_MEMBERS_BUTTON_CLASSES = [
+  'http://www.w3.org/2004/02/skos/core#Concept',
+]
+
 const urlPath = ref(getPageUrl());
 const {status, error, data} = useGetItem(runtimeConfig.public.prezApiEndpoint, urlPath);
-const isCatalog = computed(() => data.value?.data.rdfTypes?.find(n => n.value == 'http://www.w3.org/ns/dcat#Catalog'));
+const showMembersTable = computed(() => data.value?.data.rdfTypes?.find(n => SHOW_MEMBER_LIST_CLASSES.includes(n.value)));
+const hideMembersButton = computed(() => data.value?.data.rdfTypes?.find(n => HIDE_MEMBERS_BUTTON_CLASSES.includes(n.value)));
 const membersUrl = computed(() => {
   const baseMembersUrl = data.value?.data?.members?.value;
   return baseMembersUrl ? baseMembersUrl + getPageUrl().replace(/^[^?]+/, '') : '';
 });
 
 // Workaround for when membersUrl is not yet ready and thus null
-let membersData: any = null;
+const membersData: ShallowRef<any> = shallowRef(null);
+const membersError: ShallowRef<Error | null | undefined> = shallowRef(null);
 watch(membersUrl, () => {
-  if (membersData || !membersUrl.value) {
+  if (membersData || !membersUrl.value || !showMembersTable) {
     return;
   }
-  const {data} = useGetList(runtimeConfig.public.prezApiEndpoint, membersUrl);
-  membersData = data;
+  const {data, error} = useGetList(runtimeConfig.public.prezApiEndpoint, membersUrl);
+  membersData.value = data.value;
+
+  watch(data, v => membersData.value = v);
+  watch(error, v => {
+    membersError.value = v
+  });
 }, {
   immediate: true,
 });
 </script>
 <template>
   <ItemPage>
-    <template #item-members v-if="isCatalog && membersData?.data">
+    <template #item-members v-if="!!membersUrl && showMembersTable && !membersError">
       <div class="mt-3">
-        <h2 class="font-semibold">Collections in {{ data?.data?.label?.value || data!.data.value }}</h2>
-        <ItemList v-if="membersData.data" :list="membersData.data" :key="membersUrl" />
-        <Loading v-else />
+        <h2 class="font-semibold pt-2">Collections in {{ data?.data?.label?.value || data!.data.value }}</h2>
+        <ItemList v-if="membersData.data" :list="membersData.data" :key="membersUrl"/>
+        <Loading v-else/>
 
         <slot name="pagination" :data="data" :pagination="pagination">
-            <PrezPagination :totalItems="membersData.count" :pagination="pagination" :maxReached="membersData.maxReached" />
+          <PrezPagination :totalItems="membersData.count" :pagination="pagination"
+                          :maxReached="membersData.maxReached"/>
         </slot>
       </div>
     </template>
+    <template #item-members v-else-if="hideMembersButton"><span class="hidden"></span></template>
   </ItemPage>
 </template>
--- components/ItemTable.vue
+++ components/ItemTable.vue
@@ -6,13 +6,18 @@ import {type PrezFocusNode} from 'prez-lib';
 const props = defineProps<ItemTableProps>();
 
 const hideSingleObjectPredicates = [
-    'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
-    'http://www.w3.org/2004/02/skos/core#prefLabel',
-    'http://www.w3.org/2000/01/rdf-schema#label',
+  'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
+  'http://www.w3.org/2004/02/skos/core#prefLabel',
+  'http://www.w3.org/2000/01/rdf-schema#label',
 ];
 const hidePredicates = [
-    'http://www.opengis.net/ogc-na#targetGraph',
+  'http://www.opengis.net/ogc-na#targetGraph',
 ];
+const hidePredicatesForClass: Record<string, string[]> = {
+    'http://www.w3.org/ns/dcat#TopCatalog': [
+        'http://purl.org/dc/terms/hasPart',
+    ],
+};
 const term = props.term as PrezFocusNode;
 
 const filteredProperties = computed(() => {
@@ -25,6 +30,14 @@ const filteredProperties = computed(() => {
     }
     if (f.objects.length === 1 && hideSingleObjectPredicates.includes(f.predicate.value)) {
       return false;
+    }
+    if (term?.rdfTypes?.length) {
+      for (const rdfType of term.rdfTypes) {
+        const hidePredicatesForType = hidePredicatesForClass[rdfType.value];
+        if (hidePredicatesForType?.includes?.(f.predicate.value)) {
+          return false;
+        }
+      }
     }
     return true;
   });
