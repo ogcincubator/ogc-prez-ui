804f0a9a5d2adb04712135d26357cc741933154b
Alejandro Villar <avillar@ogc.org>
2025-05-27 13:29:15 +0200

    New navigation links

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,8 +1,8 @@
 export default defineAppConfig({
   ogcMenu: [
-    {"label": "Home", "url": "/"},
+    {"label": "Home", "url": "https://ogc.org/"},
     {"label": "Browse", "url": "/catalogs"},
     {"label": "Search", "url": "/search"},
-    {"label": "About", "url": "/about"},
+    {"label": "About", "url": "https://ogcincubator.github.io/rainbow-docs/about.html"},
   ],
 });
