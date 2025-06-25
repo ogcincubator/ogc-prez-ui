0c9305b4fadeb33b72556b6f908c42df18ec895a
Alejandro Villar <avillar@ogc.org>
2025-06-25 10:45:54 +0200

    Fix double slash in docker base URL

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -8,7 +8,7 @@ for V in NUXT_PUBLIC_PREZ_API_ENDPOINT NUXT_PUBLIC_APP_TITLE ; do
   xargs -0 -a /tmp/rename_files sed -i -r "s;@@${V}@@;${!V};g"
 done
 
-xargs -0 -a /tmp/rename_files sed -i -r "s;/base-url-oqui9inohyoh2Ouqu1ooThohnaobeeSa3aiNaeP9Aiv3ochaiXaelo5oof1Noh3u;${NUXT_APP_BASE_URL};g"
+xargs -0 -a /tmp/rename_files sed -i -r "s;/base-url-oqui9inohyoh2Ouqu1ooThohnaobeeSa3aiNaeP9Aiv3ochaiXaelo5oof1Noh3u;${NUXT_APP_BASE_URL%/};g"
 
 rm -rf /usr/share/nginx/html/*
 mkdir -p /usr/share/nginx/html"${NUXT_APP_BASE_URL}"
