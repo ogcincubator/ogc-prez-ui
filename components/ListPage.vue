479b2692bd7ade73300e4f3ae4622bffd803643c
Alejandro Villar <avillar@ogc.org>
2025-06-25 11:10:37 +0200

    Improve list page headers

---
+++ components/ListPage.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import ListPage from 'prez-ui/components/ListPage.vue';
+import {type PrezDataList} from 'prez-lib';
+
+const getHeader = (data: PrezDataList | undefined) => {
+  console.log(data);
+  if (data?.parents?.length) {
+    if (data.parents.length == 1) {
+      return "Catalogs";
+    }
+    if (data.parents.length >= 2) {
+      const parent = data!.parents[data!.parents.length - 2];
+      if (parent.label) {
+        return `Members in ${parent.label.value}`;
+      }
+    }
+  }
+  return data?.parents?.[data.parents.length - 1].segment || "Members";
+};
+
+</script>
+<template>
+  <ListPage>
+    <template #header-text="{ data }">
+      {{ getHeader(data) }}
+    </template>
+  </ListPage>
+</template>
